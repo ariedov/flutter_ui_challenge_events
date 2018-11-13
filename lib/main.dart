80c525f12ca9f54b180368adb307429e86d1a1ab
david <ariedov@gmail.com>
2018-11-13 08:01:38 +0200

    implements dynamic translation and opacity

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-  double offset = 0.0;
-  double opacity = 1.0;
-
+  List<ViewModel> models = [
+    ViewModel(
+      "assets/asia.jpg",
+      0.0,
+      1.0,
+    ),
+    ViewModel(
+      "assets/man.jpg",
+      40,
+      0.5,
+    ),
+    ViewModel(
+      "assets/trees.jpg",
+      80,
+      0.3,
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -47,22 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onHorizontalDragStart: _onDragStart,
               onHorizontalDragUpdate: _onDragUpdate,
               onHorizontalDragEnd: _onDragEnd,
-              child: Stack(
-                children: <Widget>[
-                  EventCard(
-                      image: "assets/asia.jpg",
-                      opacity: (opacity / 3).clamp(1.0 / 3, 1.0),
-                      offset: (offset / 10) + 80),
-                  EventCard(
-                      image: "assets/man.jpg",
-                      opacity: (opacity / 2).clamp(1.0 / 2, 1.0),
-                      offset: (offset / 8) + 40),
-                  EventCard(
-                      image: "assets/trees.jpg",
-                      opacity: 1.0,
-                      offset: offset),
-                ],
-              ),
+              child: Stack(children: _buildStack()),
             ),
           ],
         ),
@@ -70,22 +68,67 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  _onDragStart(DragStartDetails details) {
+  List<Widget> _buildStack() {
+    return models
+        .map((model) {
+          return EventCard(
+              image: model.image, opacity: model.opacity, offset: model.offset);
+        })
+        .toList()
+        .reversed
+        .toList();
   }
+
+  _onDragStart(DragStartDetails details) {}
 
   _onDragUpdate(DragUpdateDetails details) {
     setState(() {
-      offset += details.delta.dx;
-      opacity = (offset.abs() / 100);
+      for (var i = 0; i < models.length; ++i) {
+        final model = models[i];
+
+        if (i == 0) {
+          model.opacity = 1.0;
+          model.offset += details.delta.dx;
+          continue;
+        }
+
+        model.offset += details.delta.dx / (i * 6);
+
+        model.opacity = (model.opacity + (model.offset.abs() / (i * 5000)))
+            .clamp(0.0, 1.0);
+      }
     });
   }
 
   _onDragEnd(DragEndDetails details) {
     setState(() {
-      offset = 0.0;
-      opacity = 1.0;
+      models = [
+        ViewModel(
+          "assets/asia.jpg",
+          0.0,
+          1.0,
+        ),
+        ViewModel(
+          "assets/man.jpg",
+          40,
+          0.5,
+        ),
+        ViewModel(
+          "assets/trees.jpg",
+          80,
+          0.3,
+        )
+      ];
     });
   }
+}
+
+class ViewModel {
+  String image;
+  double offset = 0.0;
+  double opacity = 1.0;
+
+  ViewModel(this.image, this.offset, this.opacity);
 }
 
 class EventTitle extends StatelessWidget {
