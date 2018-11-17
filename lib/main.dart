8577888184182452ac537daaa8b08aa5b6d59688
david <ariedov@gmail.com>
2018-11-17 14:33:04 +0200

    adds one direction title swipe

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,33 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final String title;
-  const HomePage({Key key, this.title}) : super(key: key);
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ScrollController scrollController;
+  int position = 0;
+
+  @override
+    void initState() {
+      scrollController = ScrollController();
+      super.initState();
+    }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
         elevation: 0.0,
       ),
       body: Container(
@@ -32,10 +50,22 @@ class HomePage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TitleSwitcher(),
+            TitleSwitcher(
+              scrollController: scrollController,
+            ),
             Cards(
               onProgress: (progress, direction) {
-                print("progress: $progress; direction: $direction");
+
+                final titleHeight = (40 + 64);
+
+                final offset = scrollController.offset;
+                var newOffset = progress * titleHeight / 100;
+                if (direction == Direction.BACK) {
+                  newOffset *= -1;
+                }
+
+                print("progress: $progress; direction: $direction; offse: $offset, newOffset: $newOffset");
+                scrollController.jumpTo((position * titleHeight) + newOffset);
               },
             ),
           ],
@@ -52,9 +82,24 @@ class TitleSwitcher extends StatelessWidget {
     Title("Fashion Talk", "Kyiv", "Nov 24, 2018"),
   ];
 
+  final ScrollController scrollController;
+
+  TitleSwitcher({Key key, this.scrollController}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return EventTitle(titles[0]);
+    return SizedBox(
+      height: 100.0,
+      child: ListView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: scrollController,
+        children: <Widget>[
+          EventTitle(titles[0]),
+          EventTitle(titles[1]),
+          EventTitle(titles[2]),
+        ],
+      ),
+    );
   }
 }
 
